3a533d78b4c3ddd3dd5d3616ca8000e402a057df
noway-code <dragonfury1928@gmail.com>
2025-03-22 19:14:50 -0400

    Enhance hash table implementation with record structure and memory management

--- PA2/chash.c
+++ PA2/chash.c
@@ -4,5 +4,15 @@
 
 int main(void) {
     printf("Hello, World!!!\n");
+    hashRecord *record = (hashRecord *) malloc(sizeof(hashRecord));
+    record->hash = 0;
+    record->salary = 0;
+    record->next = NULL;
+    printf("record->hash: %d\n", record->hash);
+    printf("record->salary: %d\n", record->salary);
+    printf("record->next: %p\n", record->next);
+    free(record);
+
+
     return 0;
 }
--- PA2/chash.h
+++ PA2/chash.h
@@ -4,12 +4,19 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/**
+ * @brief Structure to store hash records.
+ *
+ * This structure represents a record in a hash table, containing
+ * a hash value, a name, a salary, and a pointer to the next record
+ * in the linked list.
+ */
 typedef struct hash_struct
 {
-    uint32_t hash; // Value produced by hashing name
-    char name[50]; // String Max Length 50
-    uint32_t salary; // Non-negative value representing annual salary
-    struct hash_struct *next; // linked list next node
+    uint32_t hash; /**< Value representing the hash of the name */
+    char name[50]; /**< String with a maximum length of 50 characters */
+    uint32_t salary; /**< Unsigned integer representing the annual salary */
+    struct hash_struct *next; /**< Pointer to the next node in the linked list */
 } hashRecord;
 
 #endif //CHASH_H
