08a149dfca2c60c0aa7cd7ad84769702236e24af
EdwardR2023 <edwardr@knights.ucf.edu>
2025-03-30 22:07:00 -0400

    Added delete function and made changes to process_command to test insert and delete functions

--- PA2/chash.c
+++ PA2/chash.c
@@ -94,6 +94,69 @@ void insert(const char *name, uint32_t salary) {
 // Global read/write lock instance
 rwlock_t rwlock;
 
+// Delete Record from Hash Table
+void delete(const char *name) {
+    // Compute the hash value for the given key.
+    uint32_t hash = jenkins_hash(name);
+
+    // Acquire the write lock for the appropriate bucket.
+    rwlock_acquire_writelock(&table.locks[hash]);
+
+    // Set up pointers to traverse the linked list.
+    hashRecord *current = table.buckets[hash];
+    hashRecord *prev = NULL;
+
+    // Search for the record with the matching name.
+    while (current != NULL) {
+        if (strcmp(current->name, name) == 0) {
+            // If the record is found, remove it from the list.
+            if (prev == NULL) {
+                // Deleting the first node in the list.
+                table.buckets[hash] = current->next;
+            } else {
+                prev->next = current->next;
+            }
+            printf("Deleted: %s\n", name);
+            free(current);
+            rwlock_release_writelock(&table.locks[hash]);
+            return;
+        }
+        prev = current;
+        current = current->next;
+    }
+
+    // If the record is not found, release the lock and return.
+    rwlock_release_writelock(&table.locks[hash]);
+}
+
+//This was made to test the insert and delete commands. Print and search can be added to test implementation
+void* process_command(void *arg) {
+    char *cmd = (char *)arg;
+
+    // Make a copy because strtok modifies the string
+    char *line_copy = strdup(cmd);
+    char *token = strtok(line_copy, ",");
+
+    if (strcmp(token, "insert") == 0) {
+        char *name = strtok(NULL, ",");
+        char *salary_str = strtok(NULL, ",");
+        uint32_t salary = (uint32_t)atoi(salary_str);
+        insert(name, salary);
+    }
+    else if (strcmp(token, "delete") == 0) {
+        char *name = strtok(NULL, ",");
+        delete(name);
+    }
+    else {
+        // Ignore search and print for now
+    }
+
+    free(line_copy);
+    free(cmd);
+    return NULL;
+}
+
+/*
 void* process_command(void *arg) {
     char *cmd = (char *)arg;
     // Acquire readlock before processing (since we're just reading tokens)
@@ -107,6 +170,8 @@ void* process_command(void *arg) {
     free(cmd);
     return NULL;
 }
+*/
+
 
 int main(void) {
     FILE *file = fopen("commands.txt", "r");
